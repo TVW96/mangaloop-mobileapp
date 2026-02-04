8466387f8b47e99e4302a6450ca412992da64863
Troy Washington <103076891+TVW96@users.noreply.github.com>
2026-02-04 01:55:53 -0800

    Initial Commit: Style, Router, Index, Assets

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
 import { useRouter } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function Home() {
   const router = useRouter();
@@ -8,7 +9,8 @@ export default function Home() {
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={() => router.push("./index")}>
-        <Text>Back</Text>
+        <Text style={styles.title}>Home</Text>
+        <FontAwesome name="gear" size={20} color="#000" />
       </TouchableOpacity>
     </View>
   );
